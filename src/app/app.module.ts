47815184521e43de8cc6beb20a787feafb8b47e2
Xavier Xie <xieyuehao@gmail.com>
2017-04-06 23:21:31 +1000

    1. Remove mock-hero 2. Add http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router'; //angular router
+
+import { AppRoutingModule } from './app-routing.module';
+
+//imports for loading & configuring the in-memory web aip
+import { InMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./in-memory-data.service";
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard.component';
@@ -9,12 +16,13 @@ import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 
-import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
@@ -23,7 +31,7 @@ import { AppRoutingModule }     from './app-routing.module';
     HeroDetailComponent,
     HeroesComponent
   ],
-    providers: [ HeroService ],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
