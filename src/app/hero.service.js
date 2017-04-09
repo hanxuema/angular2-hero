4999646bb71d41afd9cbc222f14c7920f54f7f78
Xavier Xie <xieyuehao@gmail.com>
2017-04-09 23:19:09 +1000

    Added http service Added "Add function" Added "Delete function" Added "Update function"

--- src/app/hero.service.js
+++ src/app/hero.service.js
@@ -5,19 +5,63 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
-var mock_heroes_1 = require("./mock-heroes");
+var http_1 = require("@angular/http");
+//There are many operators like toPromise that extend Observable with useful capabilities. To use those capabilities, you have to add the operators themselves. That's as easy as importing them from the RxJS library like this:
+require("rxjs/add/operator/toPromise");
 var HeroService = (function () {
-    function HeroService() {
+    function HeroService(http) {
+        this.http = http;
+        this.heroesUrl = 'api/heroes'; //url to web api
+        this.headers = new http_1.Headers({ 'content-Type': 'application/json' });
     }
-    HeroService.prototype.getHero = function (id) {
-        return this.getHeroes().then(function (heroes) { return heroes.find(function (hero) { return hero.id === id; }); });
+    //update a hero 
+    HeroService.prototype.update = function (hero) {
+        var url = '${this.heroesUrl}/${hero.id}';
+        return this.http
+            .put(url, JSON.stringify(hero), { headers: this.headers })
+            .toPromise()
+            .then(function () { return hero; })
+            .catch(this.handleError);
     };
+    //add new hero to the web api
+    HeroService.prototype.create = function (name) {
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({ name: name }), { headers: this.headers })
+            .toPromise()
+            .then(function (res) { return res.json().data; })
+            .catch(this.handleError);
+    };
+    //delete a hero from web api
+    HeroService.prototype.delete = function (id) {
+        var url = '${this.heroesUrl}/${id}';
+        return this.http.delete(url, { headers: this.headers })
+            .toPromise()
+            .then(function () { return null; })
+            .catch(this.handleError);
+    };
+    //get single hero by id from web api
+    HeroService.prototype.getHero = function (id) {
+        var url = '${this.heroesUrl}/${id}';
+        return this.http.get(url)
+            .toPromise()
+            .then(function (response) { return response.json().data; })
+            .catch(this.handleError);
+    };
+    //get all heroes from web api
     HeroService.prototype.getHeroes = function () {
-        return Promise.resolve(mock_heroes_1.HEROES);
+        //http.get returns an RxJS Observables are a powerful way to manage asynchronous data flows.
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(function (response) { return response.json().data; })
+            .catch(this.handleError);
     };
     ;
+    //call the getHeroes method just simulate server latency with 2 second delay
     HeroService.prototype.getHeroesSlowly = function () {
         var _this = this;
         return new Promise(function (resolve) {
@@ -25,10 +69,15 @@ var HeroService = (function () {
             setTimeout(function () { return resolve(_this.getHeroes()); }, 2000);
         });
     };
+    HeroService.prototype.handleError = function (error) {
+        console.error('An error occurred', error); //for demo purposes only
+        return Promise.reject(error.message || error);
+    };
     return HeroService;
 }());
 HeroService = __decorate([
-    core_1.Injectable()
+    core_1.Injectable(),
+    __metadata("design:paramtypes", [http_1.Http])
 ], HeroService);
 exports.HeroService = HeroService;
 //# sourceMappingURL=hero.service.js.map
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,22 +1,73 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+//There are many operators like toPromise that extend Observable with useful capabilities. To use those capabilities, you have to add the operators themselves. That's as easy as importing them from the RxJS library like this:
+import 'rxjs/add/operator/toPromise';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
-    getHero(id: number): Promise<Hero> {
-        return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
+    private heroesUrl = 'api/heroes';//url to web api
+    private headers = new Headers({ 'content-Type': 'application/json' });
+    constructor(private http: Http) { }
+
+    //update a hero 
+    update(hero: Hero): Promise<Hero> {
+        const url = '${this.heroesUrl}/${hero.id}';
+        return this.http
+            .put(url, JSON.stringify(hero), { headers: this.headers })
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
     }
 
+    //add new hero to the web api
+    create(name: string): Promise<Hero> {
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({ name: name }), { headers: this.headers })
+            .toPromise()
+            .then(res => res.json().data as Hero)
+            .catch(this.handleError);
+    }
+
+    //delete a hero from web api
+    delete(id: number): Promise<void> {
+        const url = '${this.heroesUrl}/${id}';
+        return this.http.delete(url, { headers: this.headers })
+            .toPromise()
+            .then(() => null)
+            .catch(this.handleError);
+    }
+
+    //get single hero by id from web api
+    getHero(id: number): Promise<Hero> {
+        const url = '${this.heroesUrl}/${id}';
+        return this.http.get(url)
+            .toPromise()
+            .then(response => response.json().data as Hero)
+            .catch(this.handleError);
+    }
+
+    //get all heroes from web api
     getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
+        //http.get returns an RxJS Observables are a powerful way to manage asynchronous data flows.
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            .catch(this.handleError);
     };
 
+    //call the getHeroes method just simulate server latency with 2 second delay
     getHeroesSlowly(): Promise<Hero[]> {
         return new Promise(resolve => {
             //simulate server latency with 2 second delay
             setTimeout(() => resolve(this.getHeroes()), 2000);
         })
 
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error);//for demo purposes only
+        return Promise.reject(error.message || error);
     }
 }
