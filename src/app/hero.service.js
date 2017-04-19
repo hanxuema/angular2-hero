69a544d88281eda095fa12e85168ad94e71740aa
Xavier <xieyuehao@gmail.com>
2017-04-20 09:36:48 +1000

    ds

--- src/app/hero.service.js
+++ src/app/hero.service.js
@@ -70,7 +70,7 @@ var HeroService = (function () {
         });
     };
     HeroService.prototype.handleError = function (error) {
-        console.error('An error occurred', error); //for demo purposes only
+        console.error('An error occurred', error); // for demo purposes only
         return Promise.reject(error.message || error);
     };
     return HeroService;
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -68,7 +68,7 @@ export class HeroService {
     }
 
     private handleError(error: any): Promise<any> {
-        console.error('An error occurred', error);//for demo purposes only
+        console.error('An error occurred', error); // for demo purposes only
         return Promise.reject(error.message || error);
     }
 }
